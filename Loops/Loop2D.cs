7d2f133d7cd88f444f53c2400475743bd9cf9ad7
Grimnir Ilskason <grim@fb.com>
2019-05-15 12:20:41 +0100

    added LODˆ

--- Loops/Loop2D.cs
+++ Loops/Loop2D.cs
@@ -4,10 +4,17 @@ namespace Grimity.Loops {
 class Loop2D {
     private readonly int _xMax;
     private readonly int _yMax;
+    private readonly Func<int, int> _increment = (int x) => x + 1;
 
     public Loop2D(int yMax, int xMax) {
         _xMax = xMax;
         _yMax = yMax;
+    }
+
+    public Loop2D(int yMax, int xMax, Func<int, int> increment) {
+        _xMax = xMax;
+        _yMax = yMax;
+        _increment = increment;
     }
 
     public Loop2D(int max) {
@@ -20,8 +27,8 @@ class Loop2D {
     /// </summary>
     /// <param name="action"></param>
     public void loopY(Action<int, int> action) {
-        for (var x = 0; x < _yMax; x++) {
-            for (var y = 0; y < _xMax; y++) {
+        for (var x = 0; x < _yMax; x = _increment(x)) {
+            for (var y = 0; y < _xMax; y = _increment(y)) {
                 action(x, y);
             }
         }
@@ -34,8 +41,8 @@ class Loop2D {
     /// </summary>
     /// <param name="action"></param>
     public void loopX(Action<int, int> action) {
-        for (var y = 0; y < _xMax; y++) {
-            for (var x = 0; x < _yMax; x++) {
+        for (var y = 0; y < _xMax; y = _increment(y)) {
+            for (var x = 0; x < _yMax; x = _increment(x)) {
                 action(x, y);
             }
         }
--- Mesh/MeshGenerator.cs
+++ Mesh/MeshGenerator.cs
@@ -8,27 +8,40 @@ public class MeshGenerator {
                                                 float[,] heightMap,
                                                 float heightAmplifier,
                                                 AnimationCurve heightCurve,
+                                                int levelOfDetail = 0,
                                                 float distanceBetweenVertices = 1f) {
+        var increment = levelOfDetail == 0 ? 1 : levelOfDetail * 2;
+
+        var widthVerticesCount = (width - 1) / increment + 1;
+        var heightVerticesCount = (height - 1) / increment + 1;
+
         // VERTICES & UVS ------------
-        var vertices = new Vector3[height * width];
-        var uvs = new Vector2[height * width];
+        var vertices = new Vector3[widthVerticesCount * heightVerticesCount];
+        var uvs = new Vector2[widthVerticesCount * heightVerticesCount];
         var index = 0;
 
-        new Loop2D(width, height).loopY((x, y) => {
+        new Loop2D(width, height, i => i + increment).loopY((x, y) => {
             var elevation = heightCurve.Evaluate(heightMap[x, y]) * heightAmplifier;
             vertices[index] = new Vector3(x * distanceBetweenVertices, elevation, y * distanceBetweenVertices);
             uvs[index] = new Vector2(x / (float) width, y / (float) height);
             index++;
         });
 
+        /**
+         *
+         * 0-2-4-6
+         * 
+         * C-C-C-CA-A-A-AB-B-B-B
+         */
+
         // TRIANGLES ------------
-        var triangles = new int[3 * 2 * (width - 1) * (height - 1)];
+        var triangles = new int[3 * 2 * (widthVerticesCount - 1) * (heightVerticesCount - 1)];
         index = 0;
-        new Loop2D(width - 1, height - 1).loopX((x, y) => {
-            var topLeft = height * x + y;
+        new Loop2D(widthVerticesCount - 1, heightVerticesCount - 1).loopX((x, y) => {
+            var topLeft = heightVerticesCount * x + y;
             var topRight = topLeft + 1;
-            var bottomRight = topRight + height;
-            var bottomLeft = topLeft + height;
+            var bottomRight = topRight + heightVerticesCount;
+            var bottomLeft = topLeft + heightVerticesCount;
             triangles[index++] = topLeft;
             triangles[index++] = bottomRight;
             triangles[index++] = bottomLeft;
