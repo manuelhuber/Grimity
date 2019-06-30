cf16b909b4e57cfdab8eaed32d12395683d1486e
Manuel Huber <dev@manuel-huber.de>
2019-07-01 00:01:59 +0100

    added turrets

--- GameObjects/Geometry.cs
+++ GameObjects/Geometry.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 namespace Grimity.GameObjects {
 public class Geometry {
     public static Vector3 LowerCenter(GameObject gameObject) {
-        var meshFilter = gameObject.GetComponent<MeshFilter>();
-        var bounds = meshFilter.sharedMesh.bounds;
+        var bounds = CalculateBounds(gameObject);
         return bounds.center + new Vector3(0, -bounds.extents.y, 0);
+    }
+
+    public static Bounds CalculateBounds(GameObject gameObject) {
+        var bounds = new Bounds();
+        bounds.size = Vector3.zero; // reset
+        var colliders = gameObject.GetComponentsInChildren<Collider2D>();
+        foreach (var col in colliders) {
+            bounds.Encapsulate(col.bounds);
+        }
+
+        return bounds;
     }
 }
 }
