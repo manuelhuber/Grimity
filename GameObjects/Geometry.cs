b5e23deb760870630058e8f0539d4cf3fb060b5c
Grim <dev@manuel-huber.de>
2020-04-24 17:31:50 +0100

    Fixed various issues with building placement

--- GameObjects/Geometry.cs
+++ GameObjects/Geometry.cs
@@ -1,16 +1,20 @@
+using Ludiq.Peek;
 using UnityEngine;
 
 namespace Grimity.GameObjects {
 public class Geometry {
     public static Vector3 LowerCenter(GameObject gameObject) {
         var bounds = CalculateBounds(gameObject);
-        return bounds.center + new Vector3(0, -bounds.extents.y, 0);
+        return new Vector3(bounds.center.x, bounds.center.y - bounds.extents.y, bounds.center.z);
     }
 
     public static Bounds CalculateBounds(GameObject gameObject) {
         var bounds = new Bounds();
+        gameObject.CalculateBounds(out bounds, Space.World);
+        return bounds;
+        bounds.center = gameObject.transform.position;
         bounds.size = Vector3.zero; // reset
-        var colliders = gameObject.GetComponentsInChildren<Collider2D>();
+        var colliders = gameObject.GetComponentsInChildren<Collider>();
         foreach (var col in colliders) {
             bounds.Encapsulate(col.bounds);
         }
