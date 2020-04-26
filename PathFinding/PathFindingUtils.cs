92b5262e56435ae0689101f8d9b97d7968999ed9
Grim <dev@manuel-huber.de>
2020-04-26 10:56:27 +0100

    Improved pathfinding for bad locations

---
+++ PathFinding/PathFindingUtils.cs
@@ -0,0 +1,13 @@
+using Unity.Mathematics;
+
+namespace Grimity.PathFinding {
+public static class PathFindingUtils {
+    public static int ManhattanDistanceDiagonal(int fromX, int fromZ, int toX, int toZ, int moveStraightCost,
+                                                int moveDiagonalCost) {
+        var xDistance = math.abs(fromX - toX);
+        var yDistance = math.abs(fromZ - toZ);
+        var remaining = math.abs(xDistance - yDistance);
+        return moveDiagonalCost * math.min(xDistance, yDistance) + moveStraightCost * remaining;
+    }
+}
+}
