aba8fb55029893c584f68d1c759c8e585c3515d4
Grim <dev@manuel-huber.de>
2020-08-23 11:09:38 +0100

    Reworked "Placeable" to use mapManager (and removed old vertex manipulation / terrain flatten code etc) Organised building prefab structure

--- Layer/LayerUtil.cs
+++ Layer/LayerUtil.cs
@@ -5,5 +5,16 @@ public static class LayerUtil {
     public static bool Contains(this LayerMask mask, int layer) {
         return mask == (mask | (1 << layer));
     }
+
+    public static int LayerMaskToLayer(LayerMask layerMask) {
+        var layerNumber = 0;
+        var layer = layerMask.value;
+        while (layer > 0) {
+            layer = layer >> 1;
+            layerNumber++;
+        }
+
+        return layerNumber - 1;
+    }
 }
 }
