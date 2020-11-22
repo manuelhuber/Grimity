0f4f2d289d7fbca3acefca3b9d8801810f47d4b8
Grim <dev@manuel-huber.de>
2020-11-22 14:49:08 +0000

    Created spawn budget system for enemies

--- Collections/IEnumerableExtensions.cs
+++ Collections/IEnumerableExtensions.cs
@@ -1,9 +1,17 @@
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Grimity.Collections {
 public static class EnumerableExtensions {
     public static HashSet<T> ToHashSet<T>(this IEnumerable<T> enumerable) {
         return new HashSet<T>(enumerable);
+    }
+
+    public static T GetRandomElement<T>(this IEnumerable<T> list) {
+        var enumerable = list as T[] ?? list.ToArray();
+        var index = Random.Range(0, enumerable.Count());
+        return enumerable.ElementAt(index);
     }
 }
 }
