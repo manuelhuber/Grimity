9f8173b5e2ade583ea693271758c8d5e5f43bed6
Grim <dev@manuel-huber.de>
2020-12-23 17:15:07 +0000

    Changed the building creator to simply use a BuildingEntry

--- String/StringExtensions.cs
+++ String/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+
 namespace Grimity.String {
 public static class StringExtensions {
     public static bool Falsy(this string s) {
@@ -6,6 +8,10 @@ public static class StringExtensions {
 
     public static bool Truthy(this string s) {
         return !s.Falsy();
+    }
+
+    public static string ToPascalCase(this string s) {
+        return Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(s).Replace(" ", string.Empty);
     }
 }
 }
