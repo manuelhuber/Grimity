98f63924ed065384af6176ff43276ddc55a28b15
Grim <dev@manuel-huber.de>
2020-12-29 15:36:19 +0000

    Made class dropdown read nicer

--- String/StringExtensions.cs
+++ String/StringExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 
 namespace Grimity.String {
@@ -12,6 +13,10 @@ public static class StringExtensions {
 
     public static string ToPascalCase(this string s) {
         return Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(s).Replace(" ", string.Empty);
+    }
+
+    public static string ToSentence(this string s) {
+        return string.Concat(s.Select(c => char.IsUpper(c) ? " " + c : c.ToString())).TrimStart(' ');
     }
 }
 }
