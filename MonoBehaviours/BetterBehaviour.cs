b83b8a8d7f75b36f5d13d40c94e93bd6390c4cf3
Grim <dev@manuel-huber.de>
2026-03-01 11:26:50 +0100

    added MonoBehaviour base class with cleanup util

---
+++ MonoBehaviours/BetterBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grimity.MonoBehaviours {
+public abstract class BetterBehaviour : MonoBehaviour {
+    private readonly List<Action> _cleanup = new();
+
+    protected void RegisterCleanup(Action action) => _cleanup.Add(action);
+
+    private void OnDestroy() {
+        foreach (var action in _cleanup) action();
+        OnDestroyed();
+    }
+
+    protected virtual void OnDestroyed() {
+    }
+}
+}
