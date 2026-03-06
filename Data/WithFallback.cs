ba0fd05c69361d547437de5a103b69db011958fa
Grim <dev@manuel-huber.de>
2026-03-06 12:51:50 +0100

    Create "data with fallback" wrapper

---
+++ Data/WithFallback.cs
@@ -0,0 +1,17 @@
+﻿namespace Grimity.Data {
+public class WithFallback<T> {
+    public T Fallback { get; set; }
+    public T Value { get; set; }
+
+    public WithFallback(T fallback = default, T value = default) {
+        Fallback = fallback;
+        Value = value;
+    }
+
+    public T Get() {
+        return Value ?? Fallback;
+    }
+
+    public static implicit operator T(WithFallback<T> withFallback) => withFallback.Get();
+}
+}
