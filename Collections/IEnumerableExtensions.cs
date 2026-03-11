15768a9e0ce91f5885d255db85d4c18086f340ca
Grim <dev@manuel-huber.de>
2026-03-11 11:37:48 +0100

    Added better shuffle methods

--- Collections/IEnumerableExtensions.cs
+++ Collections/IEnumerableExtensions.cs
@@ -21,6 +21,37 @@ public static class EnumerableExtensions {
         }
     }
 
+    [Obsolete("IEnumerable is not good for shuffle, use array or list specific functions")]
     public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> list) => list.OrderBy(_ => Random.value);
+
+    public static T[] Shuffle<T>(this T[] source) {
+        var array = (T[])source.Clone();
+        ShuffleInPlace(array);
+        return array;
+    }
+
+    public static T[] ShuffleInPlace<T>(this T[] array) {
+        for (var i = array.Length - 1; i > 0; i--) {
+            var j = Random.Range(0, i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+
+        return array;
+    }
+
+    public static List<T> Shuffle<T>(this List<T> source) {
+        var list = new List<T>(source);
+        ShuffleInPlace(list);
+        return list;
+    }
+
+    public static List<T> ShuffleInPlace<T>(this List<T> list) {
+        for (var i = list.Count - 1; i > 0; i--) {
+            var j = Random.Range(0, i + 1);
+            (list[i], list[j]) = (list[j], list[i]);
+        }
+
+        return list;
+    }
 }
 }
