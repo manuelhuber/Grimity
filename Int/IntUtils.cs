52fc5b0f74bfdfe661385a1f2a85e58a25dab807
Grim <dev@manuel-huber.de>
2026-03-24 12:32:11 +0100

    Added util function

--- Int/IntUtils.cs
+++ Int/IntUtils.cs
@@ -1,7 +1,13 @@
-﻿namespace Grimity.Int {
+﻿using UnityEngine;
+
+namespace Grimity.Int {
 public static class IntUtils {
     public static int Clamp0(this int value) {
         return value < 0 ? 0 : value;
+    }
+
+    public static bool Within(this int value, Vector2Int range) {
+        return value >= range.x && value <= range.y;
     }
 }
 }
