a91314d580cfb4ce2dc3cee9aa41250c26cb039c
Grim <dev@manuel-huber.de>
2026-04-03 09:54:57 +0200

    added util function

---
+++ Collections/IReadOnlyListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Grimity.Collections {
+public static class IReadOnlyListExtensions {
+    public static int IndexOf<T>(this IReadOnlyList<T> list, T item) {
+        for (var i = 0; i < list.Count; i++) {
+            if (list[i].Equals(item)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+}
