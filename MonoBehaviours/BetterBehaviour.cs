ef4218966f5ee8fcec10c6e5d8f4f21f541d3fec
Grim <dev@manuel-huber.de>
2026-04-03 16:36:40 +0200

    made cleanup usable outside of OnDestroy

--- MonoBehaviours/BetterBehaviour.cs
+++ MonoBehaviours/BetterBehaviour.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 namespace Grimity.MonoBehaviours {
 public abstract class BetterBehaviour : MonoBehaviour {
     private readonly List<Action> _cleanup = new();
-
     protected void RegisterCleanup(Action action) => _cleanup.Add(action);
 
     private void OnDestroy() {
-        foreach (var action in _cleanup) action();
+        Cleanup();
         OnDestroyed();
+    }
+
+    protected void Cleanup() {
+        foreach (var action in _cleanup) action();
+        _cleanup.Clear();
     }
 
     protected virtual void OnDestroyed() {
