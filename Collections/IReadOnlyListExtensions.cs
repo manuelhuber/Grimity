05cacd15f78582ce81d219c0c9d2f793df9ec3be
Grim <dev@manuel-huber.de>
2026-04-03 10:20:31 +0200

    fixed NPE

--- Collections/IReadOnlyListExtensions.cs
+++ Collections/IReadOnlyListExtensions.cs
@@ -4,7 +4,7 @@ namespace Grimity.Collections {
 public static class IReadOnlyListExtensions {
     public static int IndexOf<T>(this IReadOnlyList<T> list, T item) {
         for (var i = 0; i < list.Count; i++) {
-            if (list[i].Equals(item)) {
+            if (list[i]?.Equals(item) ?? false) {
                 return i;
             }
         }
