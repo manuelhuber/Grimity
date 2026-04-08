bd7c06d2d66da016410f0a6c48f06bbd228b0f48
Grim <dev@manuel-huber.de>
2026-04-08 14:53:20 +0200

    Tooltips now get nudged/flipped when out of bounds

---
+++ RectTransformUtils/RectTransformUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Grimity.RectTransformUtils {
+public static class RectTransformUtils {
+    public static Tuple<Vector2, Vector2> GetMinMaxWorldSpace(this RectTransform transform) {
+        var cCorners = new Vector3[4];
+        transform.GetWorldCorners(cCorners);
+        var containerMin = new Vector2(cCorners[0].x, cCorners[0].y);
+        var containerMax = new Vector2(cCorners[2].x, cCorners[2].y);
+        return new(containerMin, containerMax);
+    }
+}
+}
--- Tooltip/TooltipManager.cs
+++ Tooltip/TooltipManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Grimity.GameObjects;
+using Grimity.RectTransformUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,12 +14,13 @@ public class TooltipManager : MonoBehaviour {
     [SerializeField] private GameObject TooltipContainer;
     [SerializeField] private int margins = 5;
 
-
     private GameObject _tooltipUi;
     private RectTransform _uiRectTransform;
     private Dictionary<Type, TooltipView> _prefabMap;
     private RectTransform _mouseTracker;
     private RectTransform _tooltipContainerRectTransform;
+    private HorizontalAlignment _horizontalAlignment;
+    private VerticalAlignment _verticalAlignment;
 
     private void Awake() {
         Instance = this;
@@ -38,6 +40,7 @@ public class TooltipManager : MonoBehaviour {
     }
 
     private void Update() {
+        if (!_tooltipUi.activeSelf) return;
         var mousePos = Mouse.current.position.ReadValue();
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
             _tooltipContainerRectTransform,
@@ -46,12 +49,17 @@ public class TooltipManager : MonoBehaviour {
             out var localPoint
         );
         _mouseTracker.anchoredPosition = localPoint;
+        AdjustForBounds(_horizontalAlignment, _verticalAlignment);
     }
 
     public void ShowTooltip(TooltipData data,
         HorizontalAlignment horizontalAlignment,
         VerticalAlignment verticalAlignment) {
-        SetAnchor(horizontalAlignment, verticalAlignment);
+        _horizontalAlignment = horizontalAlignment;
+        _verticalAlignment = verticalAlignment;
+        SetAnchor(_horizontalAlignment, _verticalAlignment);
+        Canvas.ForceUpdateCanvases(); // flush layout so rect sizes are accurate
+        AdjustForBounds(_horizontalAlignment, _verticalAlignment);
         var type = data.GetType();
         TooltipView prefab = null;
         while (type != null && !_prefabMap.TryGetValue(type, out prefab))
@@ -113,6 +121,60 @@ public class TooltipManager : MonoBehaviour {
         _uiRectTransform.anchorMin = new Vector2(anchorX, anchorY);
         _uiRectTransform.pivot = new Vector2(pivotX, pivotY);
         _uiRectTransform.anchoredPosition = new Vector2(posX, posY);
+    }
+
+    private void AdjustForBounds(HorizontalAlignment hAlign, VerticalAlignment vAlign) {
+        SetAnchor(hAlign, vAlign);
+        var (containerMin, containerMax) = _tooltipContainerRectTransform.GetMinMaxWorldSpace();
+        var (tooltipMin, tooltipMax) = _uiRectTransform.GetMinMaxWorldSpace();
+
+        var mousePos = new Vector2(_mouseTracker.position.x, _mouseTracker.position.y);
+
+        var overflowX = 0f;
+        if (tooltipMin.x < containerMin.x) overflowX = containerMin.x - tooltipMin.x;
+        else if (tooltipMax.x > containerMax.x) overflowX = containerMax.x - tooltipMax.x;
+
+        var overflowY = 0f;
+        if (tooltipMin.y < containerMin.y) overflowY = containerMin.y - tooltipMin.y;
+        else if (tooltipMax.y > containerMax.y) overflowY = containerMax.y - tooltipMax.y;
+
+        if (overflowX == 0f && overflowY == 0f) return;
+
+        var correctedMin = new Vector2(tooltipMin.x + overflowX, tooltipMin.y + overflowY);
+        var correctedMax = new Vector2(tooltipMax.x + overflowX, tooltipMax.y + overflowY);
+
+        var newHAlign = hAlign;
+        var newVAlign = vAlign;
+
+        // Per-axis: if the clamped position would put the mouse inside the tooltip, flip instead
+        if (overflowX != 0f && hAlign != HorizontalAlignment.Middle) {
+            var wouldOverlapMouse = mousePos.x >= correctedMin.x && mousePos.x <= correctedMax.x;
+            if (wouldOverlapMouse)
+                newHAlign = hAlign == HorizontalAlignment.Left
+                    ? HorizontalAlignment.Right
+                    : HorizontalAlignment.Left;
+        }
+
+        if (overflowY != 0f && vAlign != VerticalAlignment.Middle) {
+            var wouldOverlapMouse = mousePos.y >= correctedMin.y && mousePos.y <= correctedMax.y;
+            if (wouldOverlapMouse)
+                newVAlign = vAlign == VerticalAlignment.Bottom
+                    ? VerticalAlignment.Top
+                    : VerticalAlignment.Bottom;
+        }
+
+        var horizontalFlip = newHAlign != hAlign;
+        var verticalFlip = newVAlign != vAlign;
+        overflowX = horizontalFlip ? 0 : overflowX;
+        overflowY = verticalFlip ? 0 : overflowY;
+
+        if (horizontalFlip || verticalFlip) {
+            SetAnchor(newHAlign, newVAlign);
+        }
+
+        var worldCorrection = new Vector3(overflowX, overflowY, 0f);
+        var localCorrection = _mouseTracker.InverseTransformVector(worldCorrection);
+        _uiRectTransform.anchoredPosition += new Vector2(localCorrection.x, localCorrection.y);
     }
 }
 }
