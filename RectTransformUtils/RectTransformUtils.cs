18a7147b0762adbaad261b7883a521d3a88c5ef5
Grim <dev@manuel-huber.de>
2026-05-13 16:49:30 +0200

    minor refactor

--- RectTransformUtils/RectTransformUtils.cs
+++ RectTransformUtils/RectTransformUtils.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 namespace Grimity.RectTransformUtils {
 public static class RectTransformUtils {
-    public static Tuple<Vector2, Vector2> GetMinMaxWorldSpace(this RectTransform transform) {
-        var cCorners = new Vector3[4];
-        transform.GetWorldCorners(cCorners);
-        var containerMin = new Vector2(cCorners[0].x, cCorners[0].y);
-        var containerMax = new Vector2(cCorners[2].x, cCorners[2].y);
-        return new(containerMin, containerMax);
+    public static (Vector2 min, Vector2 max) GetMinMaxWorldSpace(this RectTransform transform) {
+        var corners = new Vector3[4];
+        transform.GetWorldCorners(corners);
+        var containerMin = new Vector2(corners[0].x, corners[0].y);
+        var containerMax = new Vector2(corners[2].x, corners[2].y);
+        return new ValueTuple<Vector2, Vector2>(containerMin, containerMax);
     }
 }
 }
