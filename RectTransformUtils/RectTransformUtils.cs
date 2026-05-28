d8352f08ee952f2ee4a09a0dd9072877071db050
Grim <dev@manuel-huber.de>
2026-05-28 20:42:00 +0200

    Added visibility utility functions for RectTransform

--- RectTransformUtils/RectTransformUtils.cs
+++ RectTransformUtils/RectTransformUtils.cs
@@ -10,5 +10,28 @@ public static class RectTransformUtils {
         var containerMax = new Vector2(corners[2].x, corners[2].y);
         return new ValueTuple<Vector2, Vector2>(containerMin, containerMax);
     }
+
+
+    public static bool FullyVisible(RectTransform viewport, RectTransform target) {
+        var viewportRect = GetWorldRect(viewport);
+        var (min, max) = target.GetMinMaxWorldSpace();
+        return viewportRect.Contains(min) && viewportRect.Contains(max);
+    }
+
+    public static bool PartiallyVisible(RectTransform viewport, RectTransform target) {
+        var viewportRect = GetWorldRect(viewport);
+        var childRect = GetWorldRect(target);
+        return viewportRect.Overlaps(childRect);
+    }
+
+    private static Rect GetWorldRect(RectTransform rt) {
+        var corners = new Vector3[4];
+        rt.GetWorldCorners(corners);
+        // corners: [0]=bottom-left, [1]=top-left, [2]=top-right, [3]=bottom-right
+        return new Rect(corners[0].x,
+            corners[0].y,
+            corners[2].x - corners[0].x,
+            corners[2].y - corners[0].y);
+    }
 }
 }
