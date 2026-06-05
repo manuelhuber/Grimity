7a95e0874afd10dab27ceffd0fd6f10f0dfe57fb
Grim <dev@manuel-huber.de>
2026-06-05 14:53:32 +0200

    Added WeightedRandomList utility for selecting items based on weight

---
+++ Rng/WeightedRandomList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace Grimity.Rng {
+public class WeightedRandomList<T> {
+    private readonly List<(T item, int weight)> _entries = new();
+    private int _totalWeight;
+
+    public int Count => _entries.Count;
+    public int TotalWeight => _totalWeight;
+
+    public WeightedRandomList<T> Add(T item, int weight) {
+        if (weight <= 0) throw new ArgumentException("Weight must be greater than zero.", nameof(weight));
+
+        _entries.Add((item, weight));
+        _totalWeight += weight;
+        return this;
+    }
+
+    public T Pick() {
+        if (_entries.Count == 0)
+            throw new InvalidOperationException("WeightedList is empty.");
+        var roll = Random.Range(0, _totalWeight);
+
+        foreach (var (item, weight) in _entries) {
+            roll -= weight;
+            if (roll < 0)
+                return item;
+        }
+
+        return _entries[^1].item;
+    }
+}
+}
