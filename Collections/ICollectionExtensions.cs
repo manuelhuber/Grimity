77bdf42ad41d83539a677982b3fa520a3e0cb61c
Grim <dev@manuel-huber.de>
2026-08-03 13:28:34 +0200

    added util

--- Collections/ICollectionExtensions.cs
+++ Collections/ICollectionExtensions.cs
@@ -5,5 +5,9 @@ public static class ICollectionExtensions {
     public static bool IsEmpty<T>(this ICollection<T> list) {
         return list.Count == 0;
     }
+
+    public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> range) {
+        foreach (var obj in range) collection.Add(obj);
+    }
 }
 }
