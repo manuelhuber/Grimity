5ccf4298a30692070fa2059f8ab35ce41d53163a
Grim <dev@manuel-huber.de>
2026-09-17 09:39:10 +0200

    removed redundant custom extension

--- Collections/IEnumerableExtensions.cs
+++ Collections/IEnumerableExtensions.cs
@@ -5,10 +5,6 @@ using Random = UnityEngine.Random;
 
 namespace Grimity.Collections {
 public static class EnumerableExtensions {
-    public static HashSet<T> ToHashSet<T>(this IEnumerable<T> enumerable) {
-        return new HashSet<T>(enumerable);
-    }
-
     public static T GetRandomElement<T>(this IEnumerable<T> list) {
         var enumerable = list as T[] ?? list.ToArray();
         var index = Random.Range(0, enumerable.Count());
